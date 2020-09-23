ebc70a08a20c4677a59a16b852ca2ba87c4ff2c8
Amarpreet Singh <46095869+amarmanhala@users.noreply.github.com>
2020-09-22 21:21:18 -0400

    Update override.js

--- override.js
+++ override.js
@@ -1,8 +1,4 @@
-// L U  C   I    D
 // Amarpreet singh's code
-// He also happened to write this ode
-// To focus, clarity, rest, and joy
-// Which, I hope, you find in this toy
 
 import GURBANI from '/gurbani.js';
 
