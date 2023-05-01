8541b3fae63df05baf6bd420f545e34095064844
amarmanhala <amar.manhala@gmail.com>
2023-05-01 02:12:08 -0400

    Add Pauri 2,3

--- gurbani.js
+++ gurbani.js
@@ -1,19 +1,114 @@
 const JAPJISAHIB = [
+    // Pauri 1
     {
         "line": "॥ ੴ ਸਤਿ ਨਾਮੁ ਕਰਤਾ ਪੁਰਖੁ ਨਿਰਭਉ ਨਿਰਵੈਰੁ ਅਕਾਲ ਮੂਰਤਿ ਅਜੂਨੀ ਸੈਭੰ ਗੁਰ ਪ੍ਰਸਾਦਿ ॥",
         "meaning" : "God is one and is everywhere and in everyone, He is the Truth Immortal, Creator, Without Fear, Without Enmity, Unborn, and Self-Created. This understanding comes by the Guru's Grace."
     },
     {
-        "line" : "॥ ਜਪੁ ॥",
+        "line" : "ਜਪੁ ॥",
         "meaning": "Chant and Meditate."
     },
     {
-        "line": "॥ ਆਦਿ ਸਚੁ ਜੁਗਾਦਿ ਸਚੁ ॥",
+        "line": "ਆਦਿ ਸਚੁ ਜੁਗਾਦਿ ਸਚੁ ॥",
         "meaning": "God was the Truth in the beginning, and has been so in all the ages."
     },
     {
-        "line": "॥ ਸੋਚੈ ਸੋਚਿ ਨ ਹੋਵਈ ਜੇ ਸੋਚੀ ਲਖ ਵਾਰ ॥",
+        "line": "ਸੋਚੈ ਸੋਚਿ ਨ ਹੋਵਈ ਜੇ ਸੋਚੀ ਲਖ ਵਾਰ ॥",
         "meaning": "Simply by contemplating on God, we cannot understand Him, even though we may think hundreds of thousands of times."
+    },
+    {
+        "line": "ਚੁਪੈ ਚੁਪ ਨ ਹੋਵਈ ਜੇ ਲਾਇ ਰਹਾ ਲਿਵ ਤਾਰ ॥",
+        "meaning": "By keeping silent (Thinking), the mind does not receive satisfaction even though it thinks with constant absorption."
+    },
+    {
+        "line": "ਭੁਖਿਆ ਭੁਖ ਨ ਉਤਰੀ ਜੇ ਬੰਨਾ ਪੁਰੀਆ ਭਾਰ ॥",
+        "meaning": "Hunger for God is not appeased - Knowledge of God is not obtained, though one may study the relevant literature of the world."
+    },
+    {
+        "line": "ਸਹਸ ਸਿਆਣਪਾ ਲਖ ਹੋਹਿ ਤ ਇਕ ਨ ਚਲੈ ਨਾਲਿ ॥",
+        "meaning": "We may have a hundred thousand folds of wisdom, even this does not help us understand God. (At the time of death intellectual smartness also stays behind)"
+    },
+    {
+        "line": "ਕਿਵ ਸਚਿਆਰਾ ਹੋਈਐ ਕਿਵ ਕੂੜੈ ਤੁਟੈ ਪਾਲਿ ॥",
+        "meaning": "Then, how can we know the Truth (The True One), and how can we remove our doubts?"
+    },
+    {
+        "line": "ਹੁਕਮਿ ਰਜਾਈ ਚਲਣਾ ਨਾਨਕ ਲਿਖਿਆ ਨਾਲਿ ॥",
+        "meaning": "Nanak says, the answer is to stay in the Will of God (doubts will clear up and we will attain Him)."
+    },
+    // Pauri 2
+    {
+        "line": "ਹੁਕਮੀ ਹੋਵਨਿ ਆਕਾਰ ਹੁਕਮੁ ਨ ਕਹਿਆ ਜਾਈ ॥",
+        "meaning": "All forms get created by His inexpressible will."
+    },
+    {
+        "line": "ਹੁਕਮੀ ਹੋਵਨਿ ਜੀਅ ਹੁਕਮਿ ਮਿਲੈ ਵਡਿਆਈ ॥",
+        "meaning": "By His Will the living-beings get created, and with His Will these obtain honor."
+    },
+    {
+        "line": "ਹੁਕਮੀ ਉਤਮੁ ਨੀਚੁ ਹੁਕਮਿ ਲਿਖਿ ਦੁਖ ਸੁਖ ਪਾਈਅਹਿ ॥",
+        "meaning": "Highs and lows; joys and sorrows are His will."
+    },
+    {
+        "line": "ਇਕਨਾ ਹੁਕਮੀ ਬਖਸੀਸ ਇਕਿ ਹੁਕਮੀ ਸਦਾ ਭਵਾਈਅਹਿ ॥",
+        "meaning": "Some, by His Command, are blessed and forgiven; others, by His Command, wander aimlessly forever."
+    },
+    {
+        "line": "ਹੁਕਮੈ ਅੰਦਰਿ ਸਭੁ ਕੋ ਬਾਹਰਿ ਹੁਕਮ ਨ ਕੋਇ ॥",
+        "meaning": "Everyone is subject to His Command; no one is beyond His Command."
+    },
+    {
+        "line": "ਨਾਨਕ ਹੁਕਮੈ ਜੇ ਬੁਝੈ ਤ ਹਉਮੈ ਕਹੈ ਨ ਕੋਇ ॥",
+        "meaning": "Nanak says, one who understands His Command, his self pride/ego will disappear and humility will emerge."
+    },
+    // Pauri 3
+    {
+        "line": "ਗਾਵੈ ਕੋ ਤਾਣੁ ਹੋਵੈ ਕਿਸੈ ਤਾਣੁ ॥",
+        "meaning": "Only those can sing of His Greatness, who have the power to do so (one who is blessed)."
+    },
+    {
+        "line": "ਗਾਵੈ ਕੋ ਦਾਤਿ ਜਾਣੈ ਨੀਸਾਣੁ ॥",
+        "meaning": "Some take His Boons as signs of His existence and sing of His praise."
+    },
+    {
+        "line": "ਗਾਵੈ ਕੋ ਗੁਣ ਵਡਿਆਈਆ ਚਾਰ ॥",
+        "meaning": "Some sing of His praise on beholding His great qualities."
+    },
+    {
+        "line": "ਗਾਵੈ ਕੋ ਵਿਦਿਆ ਵਿਖਮੁ ਵੀਚਾਰੁ ॥",
+        "meaning": "Some sing of knowledge obtained of Him, through difficult philosophical studies."
+    },
+    {
+        "line": "ਗਾਵੈ ਕੋ ਸਾਜਿ ਕਰੇ ਤਨੁ ਖੇਹ ॥ ਗਾਵੈ ਕੋ ਜੀਅ ਲੈ ਫਿਰਿ ਦੇਹ ॥",
+        "meaning": "Some sing (impressed by the fact) that He creates and then destroys it. Some sing (impressed by the fact) that God has the power to takes away life and then restore it too."
+    },
+    {
+        "line": "ਗਾਵੈ ਕੋ ਜਾਪੈ ਦਿਸੈ ਦੂਰਿ ॥",
+        "meaning": "Some sing that He (God) seems so very far away."
+    },
+    {
+        "line": "ਗਾਵੈ ਕੋ ਵੇਖੈ ਹਾਦਰਾ ਹਦੂਰਿ ॥",
+        "meaning": "Some sing that He beholds them - They are in His presence."
+    },
+    {
+        "line": "ਕਥਨਾ ਕਥੀ ਨ ਆਵੈ ਤੋਟਿ ॥ ਕਥਿ ਕਥਿ ਕਥੀ ਕੋਟੀ ਕੋਟਿ ਕੋਟਿ ॥",
+        "meaning": "There are many who sing His praise. (There is no dearth of views about God. There is no end to those who give opinions about God. Thinkers tell about Him in many ways that He is). Many praise Him. (Millions give descriptions of God)."
+    },
+    {
+        "line": "ਦੇਦਾ ਦੇ ਲੈਦੇ ਥਕਿ ਪਾਹਿ ॥",
+        "meaning": "The Giver (God) keeps on giving, though the recipients may become weary of receiving."
+    },
+    {
+        "line": "ਜੁਗਾ ਜੁਗੰਤਰਿ ਖਾਹੀ ਖਾਹਿ ॥",
+        "meaning": "Since ages the people have been eating and consuming (His provisions)."
+    },
+    {
+        "line": "ਹੁਕਮੀ ਹੁਕਮੁ ਚਲਾਏ ਰਾਹੁ ॥",
+        "meaning": "The Lord sets up (Establishes) everything within His System (Discipline, Order)."
+    },
+    {
+        "line": "ਨਾਨਕ ਵਿਗਸੈ ਵੇਪਰਵਾਹੁ ॥",
+        "meaning": "Nanak, God stays in joy and is carefree (The world continues in the System He has set)."
     }
 ]
 
